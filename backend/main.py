324a88c11e3b12d3673a9f967ba181549e87f63f
biganton <134560121+biganton@users.noreply.github.com>
2024-04-29 16:21:11 +0200

    dockerfile change, connection to DB fixed

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,6 @@ username = "BD_415123"
 password = "dupa123"
 host = "dbmanage.lab.ii.agh.edu.pl"
 port = "1521"
-service_name = "DBMANAGE"
 dsn = cx_Oracle.makedsn(host, port, sid="DBMANAGE")
 
 @contextmanager
@@ -19,25 +18,43 @@ def get_db_connection():
     finally:
         connection.close()
 
-@contextmanager
-def get_db_cursor(connection):
-    cursor = connection.cursor()
-    try:
-        yield cursor
-    finally:
-        cursor.close()
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/test_db")
-def test_db(connection: cx_Oracle.Connection = Depends(get_db_connection)):
-    with get_db_cursor(connection) as cursor:
-        cursor.execute("SELECT 'Success' FROM DUAL")
-        result = cursor.fetchone()
+def test_db():
+    with get_db_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 'Success' FROM DUAL")
+            result = cursor.fetchone()
     return {"db_test": result}
 
 @app.get("/reservations")
 def read_reservations():
-    return {"reservations": ["Reservation 1", "Reservation 2"]}
+    try:
+        with get_db_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    SELECT reservation_id, table_id, customer_id, start_date, end_date, no_guests, status, notes 
+                    FROM reservations
+                """)
+                reservations = cursor.fetchall()
+                reservations_list = [
+                    {
+                        "reservation_id": row[0],
+                        "table_id": row[1],
+                        "customer_id": row[2],
+                        "start_date": row[3],
+                        "end_date": row[4],
+                        "no_guests": row[5],
+                        "status": row[6],
+                        "notes": row[7]
+                    }
+                    for row in reservations
+                ]
+        return {"reservations": reservations_list}
+    except HTTPException as e:
+        return e.detail
+
+
